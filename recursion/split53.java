417adc1148c3c4842d94bad4cb4e77e921a372ec
Kevin Silberblatt <ksilberblatt@cox.net>
2018-06-18 19:07:58 -0700

    Adds split53 java solution

---
+++ recursion/split53.java
@@ -0,0 +1,23 @@
+public boolean split53(int[] nums) {
+  int index = 0;
+  int sum1 = 0;
+  int sum2 = 0;
+  return recArray(nums, index, sum1, sum2);
+}
+
+private boolean recArray ( int[] nums, int index, int sum1, int sum2 ) {
+  if ( index >= nums.length ) {
+    return sum1 == sum2;
+  }
+
+  int value = nums[index];
+  if (value % 5 == 0 || value % 3 == 0){
+    if (value % 5 == 0) {
+      return recArray(nums, index + 1, sum1 + value, sum2);
+    }
+    return recArray(nums, index + 1, sum1, sum2 + value);
+  }
+
+  return (recArray(nums, index + 1, sum1 + value, sum2) ||
+    recArray(nums, index + 1, sum1, sum2 + value));
+}
